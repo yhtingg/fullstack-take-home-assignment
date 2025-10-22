ddad7272f8b6009e0764795ec08897a622eec212
Hui Ting <huitingxd@gmail.com>
2025-10-22 17:53:52 +0800

    Make percentageDiff red when < 0

--- boundless-assignment/app/coins/[id]/page.tsx
+++ boundless-assignment/app/coins/[id]/page.tsx
@@ -18,7 +18,7 @@ export default async function CoinPage({ params }: { params: { id: string } }) {
     <div className="mt-4">
       <h1 className="text-xl mb-2">{details.name} ({details.symbol.toUpperCase()})</h1>
       <h1 className="text-2xl">${details.market_data.current_price.usd.toLocaleString()}</h1>
-      <h3 className="text-green-700">
+      <h3 className={percentageDiff >= 0 ? "text-green-700" : "text-red-700"}>
         {percentageDiff >= 0 ? "+" : ""}
         {percentageDiff.toFixed(2)}%</h3>
 
